af51b7d4213c27862e80f98f93209f480a138723
AZLO61 <Nikola-du61@hotmail.fr>
2026-08-10 10:56:12 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import BoardGenerator from "./BoardGenerator.jsx";
+import ErrorBoundary from "./ui/ErrorBoundary.jsx";
 
-createRoot(document.getElementById("root")).render(<BoardGenerator />);
+createRoot(document.getElementById("root")).render(
+  <ErrorBoundary>
+    <BoardGenerator />
+  </ErrorBoundary>
+);
